8bb1dc3a744f5ef1bfd33c021e8b2b6c8dd1afd6
mutakoduru <balamurali1408@gmail.com>
2017-01-16 22:47:17 -0800

    twitter application

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: '5nLHt5HvjUDJeolJR6oyVs5FZ',
+		consumer_secret: 'QzIAdnD1aN93RaINF5Sr44Ip8TGPZLNEtR5MMjpvsWgHGm48Oe',
+		access_token: '821243259456397312-USSvrkYVMcibLse4XB3XtAKSgsXDRVL',
+		access_token_secret: 'bKL5ljkHzycL729ZnSYMO2Ab3XLFcaocEjlMqLePUPFKU'
+	}
+
+};
